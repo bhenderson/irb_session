86cd8f7af78469a48dab0ec5b7247792956511f9
bhenderson <bhenderson@attinteractive.com>
2010-12-03 07:40:13 -0800

    Figured out how to add a test for this.

--- test/test_irb_session.rb
+++ test/test_irb_session.rb
@@ -3,7 +3,29 @@ require "mocha"
 require "irb_session"
 
 class TestIrbSession < Test::Unit::TestCase
+
+  class TestError < StandardError; end
+
+  def get_binding
+    # how hacky is this?!
+    injected_binding = nil
+    IRB::Context.expects(:new).with do |*params|
+      injected_binding = params[1].binding
+      Binding === injected_binding
+    end.raises TestError
+    # this is just because I don't want to actually go into irb.
+    assert_raise TestIrbSession::TestError do
+      IrbSession.start_irb
+    end
+    injected_binding
+  end
+
   def test_binding_being_correctly_set
-    # how would you make a test for this?
+    injected_binding = get_binding
+    # injected_binding should be from IrbSession.new
+    vers = eval "VERSION", injected_binding
+    assert_equal IrbSession::VERSION, vers
+    klass_name = eval "self.name", injected_binding
+    assert_equal IrbSession.name, klass_name
   end
 end
